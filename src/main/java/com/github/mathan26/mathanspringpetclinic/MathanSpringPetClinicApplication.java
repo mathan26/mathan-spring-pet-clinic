6f9831dbdfca139cde9ed80ce43f566ef2771298
mathan26 <mathan.se@gmail.com>
2020-06-24 16:35:12 +0530

    Project Files Added

---
+++ src/main/java/com/github/mathan26/mathanspringpetclinic/MathanSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.github.mathan26.mathanspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathanSpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathanSpringPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mathan26/mathanspringpetclinic/MathanSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mathan26.mathanspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathanSpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
