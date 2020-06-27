b78a6279593d18a39d1342f13de5887f8e90dddc
mathan26 <mathan.se@gmail.com>
2020-06-27 11:59:00 +0530

    commit: adding base entity. closes #10

--- pet-clinic-data/src/main/java/com/github/mathan26/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/github/mathan26/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.github.mathan26.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/github/mathan26/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/github/mathan26/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.github.mathan26.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
