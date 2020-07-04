12da27f298f80e954d48acc08492f99e42484e91
mathan26 <mathan.se@gmail.com>
2020-07-04 15:20:47 +0530

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/github/mathan26/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.github.mathan26.petclinic.services;
+
+import com.github.mathan26.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
