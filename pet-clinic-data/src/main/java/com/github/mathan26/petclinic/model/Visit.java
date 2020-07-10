4683bacefc1ea522b5f99e19f6682cb81f45be39
mathan26 <mathan.se@gmail.com>
2020-07-10 19:06:31 +0530

    Convert Vets to JPA Entities closes #37

--- pet-clinic-data/src/main/java/com/github/mathan26/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/github/mathan26/petclinic/model/Visit.java
@@ -1,11 +1,23 @@
 package com.github.mathan26.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="visits")
 public class Visit extends BaseEntity{
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @OneToOne
     private Pet pet;
+
 
     public LocalDate getDate() {
         return date;
