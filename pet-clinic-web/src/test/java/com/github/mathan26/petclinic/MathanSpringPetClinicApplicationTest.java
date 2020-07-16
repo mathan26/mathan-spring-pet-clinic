cfbab1efc84b4a7639f624abf0046065b28364a1
mathan26 <mathan.se@gmail.com>
2020-07-16 16:44:04 +0530

    CRUD test in the OwnerMapService. closes #49

---
+++ pet-clinic-web/src/test/java/com/github/mathan26/petclinic/MathanSpringPetClinicApplicationTest.java
@@ -0,0 +1,16 @@
+package com.github.mathan26.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MathanSpringPetClinicApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
