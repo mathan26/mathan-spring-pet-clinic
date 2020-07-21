301b414f4b43e4565891765d41db56d5a61f45cb
mathan26 <mathan.se@gmail.com>
2020-07-21 11:01:45 +0530

    added add pet, update pet. Closes #56

--- pet-clinic-data/src/main/java/com/github/mathan26/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/github/mathan26/petclinic/model/Pet.java
@@ -16,6 +16,16 @@ import lombok.*;
 @Table(name = "pets")
 public class Pet  extends BaseEntity{
 
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.visits = visits;
+    }
+
     @Column(name = "name")
     private String name;
 
@@ -32,5 +42,7 @@ public class Pet  extends BaseEntity{
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
+
+
 
 }
