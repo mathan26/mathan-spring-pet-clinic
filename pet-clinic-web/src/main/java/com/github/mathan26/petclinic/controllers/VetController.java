1acf41c7d97a2f7a03f5c6d16adcf24eade3c3c5
mathan26 <mathan.se@gmail.com>
2020-07-22 16:23:38 +0530

    JSON Vet api end point created. Closes #70

--- pet-clinic-web/src/main/java/com/github/mathan26/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/github/mathan26/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.github.mathan26.petclinic.controllers;
 
+import com.github.mathan26.petclinic.model.Vet;
 import com.github.mathan26.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +23,12 @@ public class VetController {
 
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
